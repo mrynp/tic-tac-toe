476ffff1321ac6ecd5528633ec836739ed68b781
mrynp <110476086+mrynp@users.noreply.github.com>
2024-09-04 14:01:27 +0800

    Add player mark on board array when clicked

--- app.js
+++ app.js
@@ -8,39 +8,58 @@ const Gameboard = (() => {
       cellElement.id = index;
       document.querySelector("#gameboard").append(cellElement);
     });
+    const squares = document.querySelectorAll(".square");
+    squares.forEach((square) => {
+      square.addEventListener("click", Game.handleClick);
+    });
+  };
+
+  const update = (index, value) => {
+    board[index] = value;
+    console.log(board);
   };
 
   return {
     displayBoard,
+    update,
   };
 })();
 
-const Game = () => {
+const createPlayer = (name, mark) => {
+  return {
+    name,
+    mark,
+  };
+};
+
+const Game = (() => {
   let players = [];
   let currentPlayerIndex = 0;
   let gameOver = false;
 
   const start = () => {
-    players = [("Player 1", "X"), ("Player 2", "O")];
+    players = [createPlayer("Player 1", "X"), createPlayer("Player 2", "O")];
     currentPlayerIndex = 0;
     gameOver = false;
     Gameboard.displayBoard();
-
-    const squares = document.querySelectorAll(".square");
-    squares.forEach((square) => {
-      square.addEventListener("click", handleClick);
-    });
+    // const squares = document.querySelectorAll(".square");
+    // squares.forEach((square) => {
+    //   square.addEventListener("click", handleClick);
+    // });
   };
 
   const handleClick = (event) => {
     let index = parseInt(event.target.id);
     console.log(index);
+    Gameboard.update(index, players[currentPlayerIndex].mark);
+    currentPlayerIndex = currentPlayerIndex === 0 ? 1 : 0;
+    console.log(currentPlayerIndex);
   };
 
   return {
     start,
     handleClick,
   };
-};
+})();
 
-Game().start();
+Game.start();
